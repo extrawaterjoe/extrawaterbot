442e5e815845abaa6a82c8dd8ff62213d7758d20
joe lazarz <jlazarz87@gmail.com>
2022-05-24 20:13:57 -0400

    saveAsset

--- app.js
+++ app.js
@@ -11,9 +11,11 @@ const client = new TwitterApi({
   accessSecret: process.env.ACCESSSECRET,
 })
 
+let media
+
 const tweet = async () => {
   try {
-    const mediaId = await client.v1.uploadMedia('img/img.jpg')
+    const mediaId = await client.v1.uploadMedia(media)
     await client.v2.tweet({media: { media_ids: [mediaId]}})
   } catch (error) {
     console.log(error)
@@ -30,23 +32,40 @@ const download = async (url, dest) => {
   })
 }
 
-const fetchImg = async () => {
+const fetchAsset = async () => {
   const res = await fetch("http://api.are.na/v2/channels/stujio?page=1&per=1000")
   const data = await res.json()
   const rnd = Math.floor(Math.random() * data.contents.length)
-  const img = {
+  const asset = {
     id: data.contents[rnd].id,
-    class: data.contents[rnd].class,
     url: data.contents[rnd].image.large.url,
+    class: data.contents[rnd].class,
+    type: data.contents[rnd].image.content_type,
+    title: data.contents[rnd].generated_title,
   }
 
+  saveAsset(asset)
+}
+
+const saveAsset = asset => {
+  const assetClass = {
+    "Image": "img",
+  }
+  const assetType = {
+    "image/jpeg": "jpg",
+    "image/png": "png",
+    "image/gif": "gif",
+  }
+
+  
   try {
     const record = JSON.parse(fs.readFileSync("./record.json", "utf8"))
-    if (record.content.includes(img.id)) {
-      fetchImg()
+    if (record.content.includes(asset.id)) {
+      fetchAsset()
     } else {
-      download(img.url, `./img/img.jpg`)
-      record.content.push(img.id)
+      media = `./${assetClass[asset.class]}/asset.${assetType[asset.type]}`
+      download(asset.url, media)
+      record.content.push(asset.id)
       fs.writeFileSync("./record.json", JSON.stringify(record))
     }
   } catch (err) {
@@ -54,9 +73,9 @@ const fetchImg = async () => {
   }
 }
 
-// fetchImg()
+fetchAsset()
 
 // run every 3 hrs
-schedule.scheduleJob("0 */3 * * *", () => {
-  fetchImg()
-})
+// schedule.scheduleJob("0 */3 * * *", () => {
+//   fetchAsset()
+// })
